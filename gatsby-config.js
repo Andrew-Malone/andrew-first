24fa4dc04145b05c99453e83c6dedfe3c7e75dfc
bojo420 <drewandmalone@gmail.com>
2020-08-13 16:24:23 +0700

    Fixed dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 const dotenv = require("dotenv");
 
-if (process.env.ENVIRONMENT !== "production") {
-  dotenv.config();
-}
+// if (process.env.ENVIRONMENT !== "production") {
+//   dotenv.config();
+// }
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 
 const { spaceId, accessToken } = process.env;
 
